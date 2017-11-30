3e843e60fa32a5416b0ad041c8247daff6d9f95d
Sidi <sidiabidine@gmail.com>
2017-11-30 14:59:18 +0100

    adding scripts for enemy health, enemy movement, enemy movement

---
+++ Assets/Prototypes/Sidi/Scripts/EnemyHealth.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+
+public class EnemyHealth : MonoBehaviour
+{
+	
+	public float sinkSpeed = 2.5f;
+	public int scoreValue = 10;
+	public int startingHealth = 100;
+	public int currentHealth;
+
+	Animator anim;
+	AudioSource enemyAudio;
+	ParticleSystem hitParticles;
+	CapsuleCollider capsuleCollider;
+
+	bool isDead;
+	bool isSinking;
+
+    void Awake ()
+    {
+		anim = GetComponent<Animator> ();
+		enemyAudio = GetComponent<AudioSource> ();
+		hitParticles = GetComponentInChildren<ParticleSystem> ();
+		capsuleCollider = GetComponent<CapsuleCollider> ();
+
+		currentHealth = startingHealth;
+    }
+
+
+    void Update ()
+    {
+		if (isSinking) {
+			transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
+		}
+    }
+
+
+    public void TakeDamage (int amount, Vector3 hitPoint)
+    {
+		if (isDead)
+			return;
+
+		hitParticles.transform.position = hitPoint;
+		hitParticles.Play ();
+		currentHealth -= amount;
+
+		if (currentHealth <= 0) {
+			Death ();
+		}
+    }
+
+
+    void Death ()
+    {
+		isDead = true;
+
+		capsuleCollider.isTrigger = true;
+
+		anim.SetTrigger ("Dead");
+	
+		ScoreManager.score += scoreValue;
+    }
+
+
+    public void StartSinking ()
+    {
+		GetComponent <NavMeshAgent> ().enabled = false;
+		GetComponent <Rigidbody> ().isKinematic = true;
+		isSinking = true;
+
+		Destroy (gameObject, 2f);
+    }
+
+	public void RestartLevel ()
+	{
+		SceneManager.LoadScene (0);
+	}
+
+}
---
+++ Assets/Prototypes/Sidi/Scripts/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+  
+	GameObject Player;
+	PlayerHealth playerHealth;
+	EnemyHealth enemyeHealth;
+	NavMeshAgent nav;
+
+    void Awake ()
+    {
+		Player = GameObject.FindWithTag ("Player");
+		playerHealth = Player.GetComponent<PlayerHealth> ();
+		enemyeHealth = GetComponent<EnemyHealth> ();
+		nav = GetComponent<NavMeshAgent> ();
+    }
+
+
+    void Update ()
+    {	
+		if(enemyeHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+		{
+			nav.SetDestination (Player.transform.position);
+		}
+		else
+		{
+			nav.enabled = false;
+		}
+    }
+}
