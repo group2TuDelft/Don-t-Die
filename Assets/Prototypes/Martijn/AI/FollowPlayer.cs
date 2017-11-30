5701e25d499613fd19d5c564606691ef5decdfcf
MartijnSnijders <32513911+MartijnSnijders@users.noreply.github.com>
2017-11-30 14:40:56 +0100

    Added all my AI
    Praise jesus

---
+++ Assets/Prototypes/Martijn/AI/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+    public Transform player;
+    public Vector3 offset;
+    public Vector3 rotoffset;
+    //private Vector3 rotchange;
+
+
+    // Update is called once per frame
+    private void Start()
+    {
+        transform.eulerAngles = rotoffset;
+    }
+    void Update () {
+        
+        transform.position = player.position + offset;
+     
+        
+		
+	}
+}
---
+++ Assets/Prototypes/Martijn/AI/NPCMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCMovement : MonoBehaviour {
+    [SerializeField]
+    Transform goal;
+    [SerializeField] NavMeshAgent navMesh;
+
+
+	// Use this for initialization
+	void Start () {
+        navMesh = this.GetComponent<NavMeshAgent>();
+        SetDestination();
+	}
+	
+	private void SetDestination()
+    {
+        Vector3 targetvector = goal.transform.position;
+        navMesh.SetDestination(targetvector);
+    }
+    private void Update()
+    {
+        Vector3 targetvector = goal.transform.position;
+        navMesh.SetDestination(targetvector);
+    }
+
+}
