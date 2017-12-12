843c182d9ee511e860cef653c84037727244eca1
Sidi <sidiabidine@gmail.com>
2017-12-12 11:17:10 +0100

    Added a Grenad and Explosion effect:
    The grenade still does not really work but it produces an explostion effect , still need to do the physics

---
+++ Assets/Prototypes/Sidi/Scripts/Grenade.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour {
+
+	public float delay = 3f;
+	public GameObject explosionParticle;
+
+	float countdown;
+	bool hasExploded = false;
+
+	// Use this for initialization
+	void Start () {
+		countdown = delay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		countdown -= Time.deltaTime;
+
+		if (countdown <= 0f && !hasExploded) {
+			Explode ();
+			hasExploded = true;
+		}
+	}
+
+	void Explode(){
+		Instantiate (explosionParticle, transform.position, transform.rotation);
+
+		Destroy (this.gameObject);
+	}
+}
