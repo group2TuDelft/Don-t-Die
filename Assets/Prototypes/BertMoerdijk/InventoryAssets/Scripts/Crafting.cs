71ca2e6d56c700522ef24fde9a24223e2f9be582
Bert Moerdijk <bertmoer@gmail.com>
2017-12-12 10:29:42 +0100

    Inventory Reworked
    Inventory "Plug-And-Play" version.

---
+++ Assets/Prototypes/BertMoerdijk/InventoryAssets/Scripts/Crafting.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script will take care of the crafting system,
+// It will have to:
+// - Start up the crafting window with crafting buttons.
+// - Handle button clicks to craft, which in turn will require:
+//  > Search inventory function
+//  > Handle insufficient materials
+//  > Handle item deletion + creation (aka crafting)
+
+public class Crafting : MonoBehaviour {
+
+    // Required UI elements:
+    private GameObject WorkBenchPanel;
+    private GameObject FurnaceAndAnvil;
+    private GameObject FactoryChainBelt;
+    private GameObject ThreeDPrinter;
+    private GameObject WeirdAlienMachine;
+    private GameObject Canvas;
+
+    void Start ()
+    {
+        Canvas = GameObject.Find("MainCanvas");
+        WorkBenchPanel = Canvas.transform.GetChild(2).GetChild(0).gameObject;
+        FurnaceAndAnvil = Canvas.transform.GetChild(2).GetChild(1).gameObject;
+        FactoryChainBelt = Canvas.transform.GetChild(2).GetChild(2).gameObject;
+        ThreeDPrinter = Canvas.transform.GetChild(2).GetChild(3).gameObject;
+        WeirdAlienMachine = Canvas.transform.GetChild(2).GetChild(4).gameObject;
+    }
+
+    // Start up the crafting window with crafting buttons:
+    public void ActivateWorkBench()
+    {
+        if (WorkBenchPanel.activeSelf)
+        { 
+        WorkBenchPanel.SetActive(false);
+        }
+        else
+        {
+            WorkBenchPanel.SetActive(true);
+        }
+    }
+
+}
---
+++ Assets/Prototypes/BertMoerdijk/InventoryAssets/Scripts/WeaponSelection.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This function will be responsible for the selection of weapons in the first 4 slots.
+// It will handle:
+// - A visual clue as to which weapon is selected.
+// - A function to ask for which weapon is selected -> outputs id
+// - Make it possible to toggle between the weapons with key 1-4. (might be in worldinteraction?)
+
+public class WeaponSelection : MonoBehaviour {
+
+    private Inventory inv;
+    // visable slider located over the slots
+    private GameObject Slider;
+    private int activeSlotId = 0;
+    private GameObject Canvas;
+
+    void Start ()
+    {
+        Canvas = GameObject.Find("MainCanvas");
+        inv = GameObject.Find("Inventory").GetComponent<Inventory>();
+        Slider = Canvas.transform.GetChild(0).GetChild(2).GetChild(0).gameObject;
+        Slider.transform.position = inv.slots[activeSlotId].transform.position;
+        StartCoroutine("WaitAndTransform");
+    }
+    
+    // This function is needed in order to transform to the first slot panel at the start of the game
+    // This is because the position of the slot isnt defined in time otherwise. 
+    IEnumerator WaitAndTransform()
+    {
+        yield return new WaitForSeconds(1/10f);
+        Slider.transform.position = inv.slots[activeSlotId].transform.position;
+    }
+    
+    // Registers keys 1 - 4.
+    void Update ()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            UpdateSelectedWeapon(1);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            UpdateSelectedWeapon(2);
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            UpdateSelectedWeapon(3);
+        }
+        if (Input.GetKeyDown("4"))
+        {
+            UpdateSelectedWeapon(4);
+        }
+    }
+
+    private void UpdateSelectedWeapon (int key)
+    {
+        // Behind the scenes
+        activeSlotId = key - 1;
+
+        // Visual
+        Slider.transform.position  = inv.slots[activeSlotId].transform.position;
+        Slider.transform.localScale = Canvas.transform.GetChild(0).GetChild(0).GetChild(0).localScale;
+    }
+
+    public int GetWeaponID ()
+    {
+        return inv.items[activeSlotId].ID;
+    }
+
+}
