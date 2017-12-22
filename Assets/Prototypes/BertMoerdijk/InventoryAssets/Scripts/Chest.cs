9123f5da71bb10d9c3c6469f7c66ca2283f6b34a
PERSOONLIJK\Rob <robknoop10@gmail.com>
2017-12-22 08:59:37 +0100

    merging the first version of our game with the master

---
+++ Assets/Prototypes/BertMoerdijk/InventoryAssets/Scripts/Chest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour {
+
+    private GameObject ChestPanel;
+    private GameObject Canvas;
+    private GameObject ActiveChest;
+    private Inventory inv;
+    private Crafting craftingWindows;
+
+    // Use this for initialization
+    void Start()
+    {
+        inv = GameObject.Find("Inventory").GetComponent<Inventory>();
+        Canvas = GameObject.Find("MainCanvas");
+        ChestPanel = Canvas.transform.GetChild(1).GetChild(0).gameObject;
+        craftingWindows = inv.GetComponent<Crafting>();
+    }
+
+    public void interactChest (GameObject interactedObject)
+    {
+        if (ChestPanel.activeSelf == true)
+        {
+            inv.SaveChest(ActiveChest);
+            ChestPanel.gameObject.SetActive(false);
+        }
+        else
+        {
+            ActiveChest = interactedObject;
+            ChestPanel.gameObject.SetActive(true);
+            inv.InitializeChest(interactedObject);
+        }
+    }
+
+	public void close (GameObject interactedObject)
+	{
+		inv.SaveChest(ActiveChest);
+		ChestPanel.gameObject.SetActive(false);
+	}
+}
