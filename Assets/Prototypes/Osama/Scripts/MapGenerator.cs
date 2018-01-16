11acf3c73fde60300050a03b44ba90a3de007fa0
LAPTOP-IDIQCD2S\sarah <o.z1996@hotmail.com>
2018-01-16 12:38:32 +0100

    UPDATE

---
+++ Assets/Prototypes/Osama/Scripts/MapGenerator.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+    public AnimationCurve meshHeightCurve;
+
+    public int octaves;
+    [Range(0, 1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public float meshHeightMultiplier;
+    private float[,] noiseMap;
+    public bool autoUpdate;
+    public TerrainType[] regions;
+
+    private Mesh[] meshMaps;
+
+    public Mesh[] getMesh()
+    {
+        return meshMaps;
+    }
+
+    public void GenerateMap()
+    {
+        MapDisplay display = GetComponent<MapDisplay>();
+        int amountOfMeshes = display.meshRenderer.Length;
+
+        noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        float[][,] noiseMapPerMesh = new float[amountOfMeshes][,];
+
+        for(int i = 0; i < amountOfMeshes; i++)
+        {
+            noiseMapPerMesh[i] = new float[mapWidth, mapHeight];
+        }
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for(int x = 0; x < mapWidth; x++)
+            {
+                noiseMapPerMesh[0][x, y] = noiseMap[x, y];
+                noiseMapPerMesh[1][x, y] = noiseMap[x + mapWidth, y];
+                noiseMapPerMesh[2][x, y] = noiseMap[x, y + mapHeight];
+                noiseMapPerMesh[3][x, y] = noiseMap[x + mapWidth, y + mapHeight];
+
+            }
+        }
+
+        Color[][] colourMaps = new Color[amountOfMeshes][];
+        meshMaps = new Mesh[amountOfMeshes];
+        for(int i = 0; i < amountOfMeshes; i++)
+        {
+            colourMaps[i] = new Color[mapWidth * mapHeight];
+            for (int y = 0; y < mapHeight; y++)
+            {
+                for (int x = 0; x < mapWidth; x++)
+                {
+                    float currentHeight = noiseMapPerMesh[i][x, y];
+                    for (int j = 0; j < regions.Length; j++)
+                    {
+                        if (currentHeight <= regions[j].height)
+                        {
+                            colourMaps[i][y * mapWidth + x] = regions[j].colour;
+                            break;
+                        }
+                    }
+                }
+            }
+            Mesh meshMap = display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMapPerMesh[i], meshHeightMultiplier, meshHeightCurve),
+            TextureGenerator.TextureFromColourMap(colourMaps[i], mapWidth, mapHeight), i);
+            meshMaps[i] = meshMap;
+        }
+    }
+
+    void OnValidate()
+    {
+        if (mapWidth < 1) { mapWidth = 1; }
+        if (mapHeight < 1) { mapHeight = 1; }
+        if (lacunarity < 1) { lacunarity = 1; }
+        if (octaves < 0) { octaves = 0; }
+    }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color colour;
+}
---
+++ Assets/Prototypes/Osama/Scripts/PopulatingWorld.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PopulatingWorld : MonoBehaviour
+{
+
+    private void Start()
+    {
+        MapGenerator generatedMap = GetComponent<MapGenerator>();
+        generatedMap.seed = Random.Range(0, 10000000);
+        generatedMap.GenerateMap();
+
+        GameObject[] gameObjectMeshes = GameObject.FindGameObjectsWithTag("Mesh");
+
+        List<GameObject> clusterObjects = GetComponent<ClusterObjects>().GetClusterObjects(generatedMap);
+        List<GameObject> nonClusterObjects = GetComponent<NonClusterObjects>().GetNonClusterObjects(generatedMap);
+        List<GameObject> allObjects = new List<GameObject>();
+
+
+        allObjects.AddRange(nonClusterObjects);
+        allObjects.AddRange(clusterObjects);
+
+        foreach (GameObject gameObject in allObjects)
+        {
+            CalculateHeightObject(gameObject);
+            if (gameObject.name != "Chest(Clone)")
+            {
+                gameObject.GetComponent<Collider>().enabled = false;
+            }
+        }
+    }
+
+    public void CalculateHeightObject(GameObject gameObject)
+    {
+        RaycastHit hit;
+        Vector3 coordinates = gameObject.transform.position;
+
+        Ray downRay = new Ray(coordinates, Vector3.down);
+        Physics.Raycast(downRay, out hit, (1<<10));
+        
+        Debug.Log(gameObject.name + ", " + hit.collider + ", " + hit.distance);
+        coordinates.y = coordinates.y - hit.distance;
+
+        RaycastHit hit2;
+        Ray downRay2 = new Ray(coordinates, Vector3.down);
+        Physics.Raycast(downRay2, out hit2);
+        coordinates.y = coordinates.y - hit2.distance;
+
+        gameObject.transform.position = new Vector3(coordinates.x, coordinates.y, coordinates.z);
+
+    }
+
+    private void CalculateAngle(GameObject gameObject)
+    {
+        RaycastHit hit;
+        Vector3 coordinates = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        Ray downRay = new Ray(coordinates, Vector3.down);
+        Physics.Raycast(downRay, out hit);
+
+        Vector3 generalDirection = Vector3.ProjectOnPlane(hit.normal, Vector3.up).normalized;
+        Debug.Log("hit normal: " + hit.normal + " general direction: " +  generalDirection);
+        if (!(generalDirection.y == 0))
+        {
+            Vector3 preciseDirection = Vector3.ProjectOnPlane(generalDirection, hit.normal).normalized;
+            Quaternion rotation = Quaternion.LookRotation(preciseDirection, hit.normal);
+
+            gameObject.transform.rotation = rotation;
+        }
+    }
+}
