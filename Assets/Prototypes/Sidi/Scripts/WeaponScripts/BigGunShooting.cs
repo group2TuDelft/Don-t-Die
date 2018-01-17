b00dbf9f6f38d8f13749d95d75defe6c8014b59e
Bert Moerdijk <bertmoer@gmail.com>
2018-01-17 12:04:15 +0100

    17-1 update
    Latest update, all weapons added, fixed several bugs, and added random object spawning in chests.

---
+++ Assets/Prototypes/Sidi/Scripts/WeaponScripts/BigGunShooting.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+
+public class BigGunShooting : MonoBehaviour
+{
+
+    public float timeBetweenBullets;
+    public float range = 40f;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    
+
+    public float sprayFactor = 0.05f;
+    public int amountofshot = 5;
+    RuntimeAnimatorController ac;
+    public float animationoffset = 0.4f;
+    private bool shot = false;
+
+    float timer;
+    Ray shootRay = new Ray();
+    RaycastHit shootHit;
+    int shootableMask;
+    Light gunLight;
+    float effectsDisplayTime = 0.2f;
+    bool bulletHit = false;
+
+    // Ammo Related vars.
+    public int ammo_id;
+    private Inventory inv;
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GameObject.Find("Player").GetComponent<Animator>();
+        shootableMask = LayerMask.GetMask("Shootable");
+        gunLight = GetComponent<Light>();
+        inv = GameObject.Find("Inventory").GetComponent<Inventory>();
+        
+        ac = anim.runtimeAnimatorController;
+
+        // Finds the length of the shooting animation
+        for (int i = 0; i < ac.animationClips.Length; i++)
+        {
+            if (ac.animationClips[i].name == "Shotgun_Shooting")
+            {
+                timeBetweenBullets = ac.animationClips[i].length / 2;
+            }
+        }
+    }
+
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            timer = 0f;
+            shot = false;
+        }
+
+        if (Input.GetButton("Fire1") && timer >= timeBetweenBullets - animationoffset && Time.timeScale != 0 && shot == false)
+        {
+            if (inv.CheckItemInInventoryByID(ammo_id))
+            {
+                anim.SetBool("AnimHasGun", true);
+                inv.DeleteItem(ammo_id, 1);
+                for (int i = 0; i < amountofshot; i++)
+                    { 
+                    Fire(Random.value);
+                }
+                shot = true;
+            }
+            else
+            {
+                anim.SetBool("AnimHasGun", false);
+                Debug.Log("Out of Ammo");
+            }
+        }
+
+        if (timer >= timeBetweenBullets)
+        {
+            timer = 0f;
+            shot = false;
+            DisableEffects();
+        }
+    }
+
+
+    public void DisableEffects()
+    {
+        gunLight.enabled = false;
+    }
+
+    void Fire(float rand)
+    {
+        gunLight.enabled = true;
+
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.transform.Rotate(Vector3.one * sprayFactor * rand * 90);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * (range * (1 + rand));
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+}
---
+++ Assets/Prototypes/Sidi/Scripts/WeaponScripts/BowScript.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowScript : MonoBehaviour {
+    
+    private float timeBetweenShot;
+    public float animationoffset;
+    public float range = 40f;
+    public string animationName;
+    public GameObject ArrowPrefab;
+    public Transform ArrowSpawn;
+    RuntimeAnimatorController ac;
+
+    private bool shot = false;
+
+    float timer;
+
+    // Ammo Related vars.
+    public int ammo_id;
+    private Inventory inv;
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GameObject.Find("Player").GetComponent<Animator>();
+        inv = GameObject.Find("Inventory").GetComponent<Inventory>();
+        ac = anim.runtimeAnimatorController;
+
+        // Finds the length of the shooting animation
+        for (int i = 0; i < ac.animationClips.Length; i++)
+        {
+            if (ac.animationClips[i].name == animationName) 
+            {
+                timeBetweenShot = ac.animationClips[i].length;
+            }
+        }
+        
+    }
+
+    void Update()
+    {
+
+        if (Input.GetButton("Fire1"))
+        {
+            timer += Time.deltaTime;
+        }
+        else { 
+        timer = 0f;
+        shot = false;
+        }
+
+        if (Input.GetButton("Fire1") && timer >= timeBetweenShot - animationoffset && Time.timeScale != 0 && shot == false)
+        {
+
+            if (inv.CheckItemInInventoryByID(ammo_id))
+            {
+                anim.SetBool("AnimHasBow", true);
+                inv.DeleteItem(ammo_id, 1);
+                Fire();
+            }
+            else
+            {
+                anim.SetBool("AnimHasBow", false);
+                Debug.Log("Out of Ammo");
+            }
+        }
+
+        if (timer >= timeBetweenShot)
+        {
+            timer = 0f;
+            shot = false;
+        }
+
+
+    }
+
+    void Fire()
+    {
+        shot = true;
+
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            ArrowPrefab,
+            ArrowSpawn.position,
+            ArrowSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * range;
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+
+}
