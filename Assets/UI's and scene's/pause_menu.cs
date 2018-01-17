d31d97e81298e529c4f9a25966f73a816f3aef79
lorenzo presilli <lorenzopresilli@hotmail.com>
2018-01-17 20:02:02 +0100

    add some models and start of menu

---
+++ Assets/UI's and scene's/pause_menu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauzemenu : MonoBehaviour {
+
+    public static bool GameIsPauzed = false;
+    public GameObject pauseMenuUI;
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPauzed)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }	
+	}
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPauzed = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPauzed = true;
+    }
+}
