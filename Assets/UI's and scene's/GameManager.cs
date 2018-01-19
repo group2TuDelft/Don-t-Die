0c947b29f5dc41c36865f32b091c7ae574757ceb
lorenzo presilli <lorenzopresilli@hotmail.com>
2018-01-19 16:28:04 +0100

    complete menu with highscore and models

--- Assets/UI's and scene's/GameManager.cs
+++ Assets/UI's and scene's/GameManager.cs
@@ -1,20 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 
 public class GameManager : MonoBehaviour
 {
     bool gameIsOver = false;
-
     public float gameOverDelay = 2f;
     public GameObject gameOverMenuUI;
-    public void GameOver()
-    {
+    public GameObject point_sphere;
+    public Text Text_score;
+    private points_on_click points;
+
+
+    public void GameOver() {
         if (gameIsOver == false)
         {
             gameIsOver = true;
             Invoke("GameOverScreen", gameOverDelay);
+            points = point_sphere.GetComponent<points_on_click>();
+            Text_score.text = "score: " + points.points;
         }
     }
 
