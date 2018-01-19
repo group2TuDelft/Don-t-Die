8f37c9d5dcdccb7bb792af4bc70466f3ff086355
lorenzo presilli <lorenzopresilli@hotmail.com>
2018-01-19 19:11:32 +0100

    master push with menu

---
+++ Assets/Prototypes/Lorenzo/UI's and scene's/player_dead_on_click.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_dead_on_click : MonoBehaviour {
+
+    public static bool playerDead = false;
+
+    void OnMouseDown()
+    {
+        playerDead = true;
+        FindObjectOfType<GameManager>().GameOver();
+        
+    }
+}
